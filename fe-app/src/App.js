ac2e368b7b03469ccb2020f07a86dddc1f5ac21d
dltmddus <tmddus4671@gmail.com>
2024-08-26 20:14:50 +0900

    fix : app.js axios 설정 테스트트

--- fe-app/src/App.js
+++ fe-app/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LandingPage from './component/page/LandingPage';
-import JoinPage from './component/page/JoinPage';
-import LoginPage from './component/page/LoginPage';
-import FindPasswordPage from './component/page/FindPasswordPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./component/page/LandingPage";
+import JoinPage from "./component/page/JoinPage";
+import LoginPage from "./component/page/LoginPage";
+import FindPasswordPage from "./component/page/FindPasswordPage";
+
+import { useEffect } from "react";
+import axios from "axios"; // 통신 테스트
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/users/signup")
+      .then((response) => {
+        console.log("연결완료" + response.data);
+      })
+      .catch((e) => {
+        console.error("에러발생. 에러발생.", e);
+      });
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
